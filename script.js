6802bbdc7ae97c033abaa3caf241cd92e91140a5
takoyaks <97886418+takoyaks@users.noreply.github.com>
2025-04-23 13:44:47 +0800

    update_v1.0

--- script.js
+++ script.js
@@ -89,6 +89,6 @@ function downloadFormattedCSV() {
   const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
   const link = document.createElement("a");
   link.href = URL.createObjectURL(blob);
-  link.download = "formatted_output.csv";
+  link.download = "dtr_copy.csv";
   link.click();
 }
