f6632c5161e78fcbf2c43368a762f01e80c6bc17
takoyaks <97886418+takoyaks@users.noreply.github.com>
2025-04-25 12:01:12 +0800

    Update tiny.js

--- tiny.js
+++ tiny.js
@@ -3,17 +3,13 @@ tinymce.init({
     selector: 'textarea',
     plugins: [
         // Core editing features
-        'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount','code',
-        // Your account includes a free trial of TinyMCE premium features
-        // Try the most popular premium features until May 8, 2025:
-        'checklist', 'mediaembed', 'casechange', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
-    ],
-    toolbar: 'code |undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        'anchor', 'autolink', 'charmap', 'codesample', 'searchreplace', 'table','code','preview'],
+    toolbar: 'tablemergecells | undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
     tinycomments_mode: 'embedded',
-    tinycomments_author: 'Author name',
+    tinycomments_author: 'Kerbie R. Villanueva',
     mergetags_list: [
-        { value: 'First.Name', title: 'First Name' },
-        { value: 'Email', title: 'Email' },
+        { value: 'Kerbie R. Villanueva', title: 'First Name' },
+        { value: 'kerbievillanueva1108@gmail.com', title: 'Email' },
     ],
     ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
  
